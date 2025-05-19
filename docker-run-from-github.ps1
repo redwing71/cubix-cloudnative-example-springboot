150d284b21b01569d7f84cc0b9174fc2929267ad
redwing71 <zsolt.rituper@gmail.com>
2025-05-19 11:23:20 +0200

    replacing placeholder for docker

--- docker-run-from-github.ps1
+++ docker-run-from-github.ps1
@@ -1,1 +1,1 @@
-docker run -p 8080:8080 --rm --name demo -it ghcr.io/USERNAME/cubix/cloudnative/demo:actions
+docker run -p 8080:8080 --rm --name demo -it ghcr.io/redwing71/cubix/cloudnative/demo:actions
